f9f23bda03f2fa1d1d4809118849355a8a744e19
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-13 22:51:51 +0900

    23/08/13

--- carfront/src/App.test.js
+++ carfront/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import AddCar from './components/AddCar';
+import TestRenderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('open add car modal form', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText('New Car'));
+  expect(screen.getByRole('dialog')).toHaveTextContent('New car');
+});
+
+test('renders a snapshot', () => {
+  const tree = TestRenderer.create(<AddCar />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
